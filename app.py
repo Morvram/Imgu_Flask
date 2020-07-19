8e6ee61052b889578b4e1177d2677ca6c3d473d1
JRMhere <joshua6781@gmail.com>
2020-07-19 15:12:19 -0500

    migrated database

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-db = SQL("sqlite:///app.db")
+db = SQL("postgres://ldrthfhaqyelpx:3350e790b7910d24ff94ec98eb5633d54b50e00d1864c7c162209e063676d04b@ec2-52-22-216-69.compute-1.amazonaws.com:5432/d96punflgs6a2d")
 
 @app.route("/")
 @login_required
