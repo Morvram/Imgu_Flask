420382678e623144eb917e46d7cf7ec43f8ed741
J.R. Mandell <JRMhere@protonmail.ch>
2022-11-06 09:52:46 -0500

    attempting to fix

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from tempfile import mkdtemp
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 from werkzeug.utils import secure_filename
+from werkzeug.datastructures import FileStorage
 import api_handler
 import pillow_filter
 import requests
